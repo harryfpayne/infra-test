3bf6622bfedb2f6f6b1390f5b86abd16609da0da
harryfpayne <harryfpayne@gmail.com>
2025-10-25 14:37:00 +0100

    some changes

--- infra/index.ts
+++ infra/index.ts
@@ -16,7 +16,13 @@ const api_service = new digitalocean.App("api-service", {spec: {
       sourceDir: "services/api",
       dockerfilePath: "services/api/Dockerfile",
       httpPort: 8080,
-    }, {
+    }]
+  }});
+
+const pug_service = new digitalocean.App("pug-service", {spec: {
+    name: "pug-service",
+    region: digitalocean.Region.LON1,
+    services: [{
       name: "pug-site",
       instanceCount: 1,
       instanceSizeSlug: "apps-s-1vcpu-1gb",
@@ -28,5 +34,5 @@ const api_service = new digitalocean.App("api-service", {spec: {
       sourceDir: "services/pug-site",
       dockerfilePath: "services/pug-site/Dockerfile",
       httpPort: 8888,
-    }],
+    }]
   }});
